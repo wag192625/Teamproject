19308d4121af1bf9969acd73971e4e4519a2bbbb
wag192625 <wag192625@gmail.com>
2022-10-13 10:42:38 +0900

    FileUploadInfoRepository 추가

---
+++ MartketMerry/project/src/main/java/com/example/project/Controller/myPage/MyPageController.java
@@ -0,0 +1,10 @@
+package com.example.project.Controller.myPage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@Controller
+//@RequestMapping(path = "/contents/myPage")
+//public class MyPageController {
+//    private final MyPageService
+//}
